2a17c11379af7921b06753936a1da42672043668
rhemamiranda <73881060+rhemamiranda@users.noreply.github.com>
2020-11-27 19:17:28 +0800

    Update verification-page.php

--- verification-page.php
+++ verification-page.php
@@ -75,7 +75,7 @@
             </div>
             <p class="m-auto col-lg-10 col-12">A verification link was resent to:</p>
             <p class="m-auto col-lg-10 col-12" id="notice">newemail.user12345@gmail.com</p> 
-            <b class="mx-auto mt-3  col-lg-10 col-12">If the email doesn't arrive soon, check your spam folder or have us resend it again.</b>
+            <b class="mx-auto mt-3  col-lg-10 col-12">If the email doesn't arrive soon, check your spam folder.</b>
             
                 <div class="mx-auto mt-3 mb-3 col-lg-10 col-12">
                     <button class="btn btn-default m-0 col-12">GO BACK TO HOME PAGE</button>
