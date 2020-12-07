1076dfbebff6f80bc7a5e37d257792e44f007096
rhemamiranda <73881060+rhemamiranda@users.noreply.github.com>
2020-12-07 08:31:49 +0800

    Update forgot-password.php

--- forgot-password.php
+++ forgot-password.php
@@ -11,6 +11,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel = "icon" href="images/icons8-ticket-100.png" type = "image">
     <title>Forgot Password • GoBook</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
