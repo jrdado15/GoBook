8a767596d40ee1036ee905eeefb9c49fdf132aef
Luke Toledo <luketoledo61@gmail.com>
2021-01-03 23:45:26 +0800

    new change profile

--- includes/processForm.php
+++ includes/processForm.php
@@ -9,18 +9,17 @@ if(isset($_POST['save-user'])){
     // nilalagay sa folder ng images yung picture
     $target ='images/'.$profileImageName;
 
-    move_uploaded_file($_FILES['profileImage']['tmp_name'],$target);
-    // kinukuha lahat ng info at hinahanap sa db yung username ng nakalogin
     $query = "SELECT * FROM account_tbl WHERE username = '$currentusername' LIMIT 1";
-    $results = mysqli_query($db, $query);
-   
-    // nilalagyan ng image yung profile_image field sa database    
-    if(mysqli_num_rows($results) > 0){  
-                $query_update = "UPDATE account_tbl SET profile_image = '$profileImageName' WHERE username = '$currentusername' ";
-                mysqli_query($db, $query_update);  
-                
-                $_SESSION['profile_image'] = $profileImageName;
-        }
+    $results = mysqli_query($db, $query);  
 
+    if(move_uploaded_file($_FILES['profileImage']['tmp_name'],$target))
+    {
+        $query_update = "UPDATE account_tbl SET profile_image = '$profileImageName' WHERE username = '$currentusername' ";
+        if(mysqli_query($db, $query_update))
+        {
+                $_SESSION['profile_image'] = $profileImageName;
+        }  
+    }
+   
 }
 ?>
